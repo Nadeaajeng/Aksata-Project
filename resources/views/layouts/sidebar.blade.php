dc64c1727c9d3f28019655da427eb2a4aea861a3
MohamadArdias <71806525+MohamadArdias@users.noreply.github.com>
2022-04-11 22:40:02 +0700

    creat laravel

---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,60 @@
+<aside id="sidebar" class="sidebar">
+
+    <ul class="sidebar-nav" id="sidebar-nav">
+
+        <li class="nav-item">
+            <a class="nav-link " href="index.html">
+                <i class="bi bi-grid"></i>
+                <span>Dashboard</span>
+            </a>
+        </li><!-- End Dashboard Nav -->
+
+        <li class="nav-heading">Master</li>
+        <li class="nav-item">
+            {{-- class="bx bxl-apple ms-auto" gunakan untuk menambah icon --}}
+            <a class="nav-link collapsed" data-bs-target="#components-nav" data-bs-toggle="collapse" href="#">
+                <i class="bi bi-layers-fill"></i><span>Kategori</span><i></i>
+            </a>
+            <a class="nav-link collapsed" data-bs-target="#components-nav" data-bs-toggle="collapse" href="#">
+                <i class="bx bxs-cube"></i><span>Produk</span><i></i>
+            </a>
+            <a class="nav-link collapsed" data-bs-target="#components-nav" data-bs-toggle="collapse" href="#">
+                <i class="bi bi-people-fill"></i><span>Member</span><i></i>
+            </a>
+            <a class="nav-link collapsed" data-bs-target="#components-nav" data-bs-toggle="collapse" href="#">
+                <i class="bi bi-people"></i><span>Supplier</span><i></i>
+            </a>
+        </li>
+        <li class="nav-heading">Transaksi</li>
+        <li class="nav-item">
+            {{-- class="bx bxl-apple ms-auto" gunakan untuk menambah icon --}}
+            <a class="nav-link collapsed" data-bs-target="#components-nav" data-bs-toggle="collapse" href="#">
+                <i class="bi bi-truck"></i><span>Pembalian</span><i></i>
+            </a>
+            <a class="nav-link collapsed" data-bs-target="#components-nav" data-bs-toggle="collapse" href="#">
+                <i class="ri-shopping-cart-2-line"></i><span>Transaksi Penjualan</span><i></i>
+            </a>
+        </li> 
+        <li class="nav-heading">Report</li>
+        <li class="nav-item">
+            {{-- class="bx bxl-apple ms-auto" gunakan untuk menambah icon --}}
+            <a class="nav-link collapsed" data-bs-target="#components-nav" data-bs-toggle="collapse" href="#">
+                <i class="bi-layout-text-window-reverse"></i><span>Histori Penjualan</span><i></i>
+            </a>
+            <a class="nav-link collapsed" data-bs-target="#components-nav" data-bs-toggle="collapse" href="#">
+                <i class="bi-bar-chart"></i><span>Laporan</span><i></i>
+            </a>
+        </li>
+        <li class="nav-heading">System</li>
+        <li class="nav-item">
+            {{-- class="bx bxl-apple ms-auto" gunakan untuk menambah icon --}}
+            <a class="nav-link collapsed" data-bs-target="#components-nav" data-bs-toggle="collapse" href="#">
+                <i class="bi bi-person"></i><span>User</span><i></i>
+            </a>
+            <a class="nav-link collapsed" data-bs-target="#components-nav" data-bs-toggle="collapse" href="#">
+                <i class="bi bi-gear"></i><span>Setting</span><i></i>
+            </a>
+        </li>
+    </ul>
+
+</aside><!-- End Sidebar-->
