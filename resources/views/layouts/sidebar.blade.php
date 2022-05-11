93263a561a3f1e493005439b17b4899c546d2782
MohamadArdias <71806525+MohamadArdias@users.noreply.github.com>
2022-05-11 13:11:47 +0700

    Update sidebar.blade.php

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -12,46 +12,46 @@
         <li class="nav-heading">Master</li>
         <li class="nav-item">
             {{-- class="bx bxl-apple ms-auto" gunakan untuk menambah icon --}}
-            <a class="nav-link collapsed" data-bs-target="#components-nav" data-bs-toggle="collapse" href="#">
+            <a class="nav-link collapsed" data-bs-target="#components-nav"  href="#">
                 <i class="bi bi-layers-fill"></i><span>Kategori</span><i></i>
             </a>
-            <a class="nav-link collapsed" data-bs-target="#components-nav" data-bs-toggle="collapse" href="#">
+            <a class="nav-link collapsed" data-bs-target="#components-nav"  href="#">
                 <i class="bx bxs-cube"></i><span>Produk</span><i></i>
             </a>
-            <a class="nav-link collapsed" data-bs-target="#components-nav" data-bs-toggle="collapse" href="#">
+            <a class="nav-link collapsed" data-bs-target="#components-nav"  href="#">
                 <i class="bi bi-people-fill"></i><span>Member</span><i></i>
             </a>
-            <a class="nav-link collapsed" data-bs-target="#components-nav" data-bs-toggle="collapse" href="#">
+            <a class="nav-link collapsed" data-bs-target="#components-nav"  href="#">
                 <i class="bi bi-people"></i><span>Supplier</span><i></i>
             </a>
         </li>
         <li class="nav-heading">Transaksi</li>
         <li class="nav-item">
             {{-- class="bx bxl-apple ms-auto" gunakan untuk menambah icon --}}
-            <a class="nav-link collapsed" data-bs-target="#components-nav" data-bs-toggle="collapse" href="#">
+            <a class="nav-link collapsed" data-bs-target="#components-nav"  href="#">
                 <i class="bi bi-truck"></i><span>Pembalian</span><i></i>
             </a>
-            <a class="nav-link collapsed" data-bs-target="#components-nav" data-bs-toggle="collapse" href="#">
+            <a class="nav-link collapsed" data-bs-target="#components-nav"  href="#">
                 <i class="ri-shopping-cart-2-line"></i><span>Transaksi Penjualan</span><i></i>
             </a>
         </li> 
         <li class="nav-heading">Report</li>
         <li class="nav-item">
             {{-- class="bx bxl-apple ms-auto" gunakan untuk menambah icon --}}
-            <a class="nav-link collapsed" data-bs-target="#components-nav" data-bs-toggle="collapse" href="#">
+            <a class="nav-link collapsed" data-bs-target="#components-nav"  href="#">
                 <i class="bi-layout-text-window-reverse"></i><span>Histori Penjualan</span><i></i>
             </a>
-            <a class="nav-link collapsed" data-bs-target="#components-nav" data-bs-toggle="collapse" href="#">
+            <a class="nav-link collapsed" data-bs-target="#components-nav"  href="#">
                 <i class="bi-bar-chart"></i><span>Laporan</span><i></i>
             </a>
         </li>
         <li class="nav-heading">System</li>
         <li class="nav-item">
             {{-- class="bx bxl-apple ms-auto" gunakan untuk menambah icon --}}
-            <a class="nav-link collapsed" data-bs-target="#components-nav" data-bs-toggle="collapse" href="#">
+            <a class="nav-link collapsed" data-bs-target="#components-nav"  href="user">
                 <i class="bi bi-person"></i><span>User</span><i></i>
             </a>
-            <a class="nav-link collapsed" data-bs-target="#components-nav" data-bs-toggle="collapse" href="#">
+            <a class="nav-link collapsed" data-bs-target="#components-nav"  href="#">
                 <i class="bi bi-gear"></i><span>Setting</span><i></i>
             </a>
         </li>
