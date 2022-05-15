5ee392daee996436d8251d36393ee7a0d82e2187
MohamadArdias <71806525+MohamadArdias@users.noreply.github.com>
2022-05-15 22:49:54 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -30,11 +30,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get('/', function () {
+    return view('welcome');
+});
 
-Route::get('/', function() {
+Route::get('/home', function() {
     return view('layouts.master');
 });
 
