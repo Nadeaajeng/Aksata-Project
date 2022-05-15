732084a8cefeb82d4f13066c1ebdc42762e0c42d
MohamadArdias <71806525+MohamadArdias@users.noreply.github.com>
2022-05-15 22:47:51 +0700

    Revert "reset"
    This reverts commit 857f5ff74b35a1c42cc35a13392fe2cc5ffe94b6.

--- routes/web.php
+++ routes/web.php
@@ -30,11 +30,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
-Route::get('/home', function() {
+Route::get('/', function() {
     return view('layouts.master');
 });
 
